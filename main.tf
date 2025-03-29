e0ae5796e5edd0f957a21d514aaab8f118809901
justajoo0374 <justajoo0374@gmail.com>
2025-03-29 12:47:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "hcp" {
+  name         = "hcp-vm"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
